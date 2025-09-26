f3644851cab75c96d86e06b67c58e6612239f0fc
EduU0U <duterres78@gmail.com>
2025-09-26 03:14:54 -0300

    Create api.js

---
+++ src/utils/src/middleware/src/controllers/src/routes/src/frontend/src/src/api/api.js
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const API_URL = import.meta.env.VITE_API_URL;
+
+export const api = axios.create({
+  baseURL: API_URL,
+});
+
+export function setToken(token) {
+  api.defaults.headers.common['Authorization'] = `Bearer ${token}`;
+}
